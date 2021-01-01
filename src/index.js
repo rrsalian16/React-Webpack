eec4d09e160a97024f2b02203807e25d9c4d41fd
rrsalian16 <rrsalian16@gmail.com>
2021-01-01 16:55:53 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/style.css';
+import './style/style.scss';
+
+
+const App=()=>(
+    <div>
+        <h1>Hello React........</h1>
+    </div>
+);
+
+ReactDOM.render(<App/>, document.getElementById("root"))
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins:[
+      new HtmlWebpackPlugin({
+          template: "public/index.html"
+      })
+  ]
+};
